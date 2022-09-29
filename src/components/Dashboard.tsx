11c5860868197739fee8927f9062cd4e44375328
TomoyukiWakiyama <kreva2002@gmail.com>
2022-09-29 22:11:09 +0900

    202209292211

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -14,7 +14,7 @@ import {
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import Header from "./parts/Header";
-import { getTasks, createTask } from "../service/api";
+import { getSnapTasks, createTask, deleteTask } from "../service/api";
 
 type TypeTasks = {
   id: number;
@@ -26,71 +26,39 @@ const Dashboard: React.FC = () => {
 
   const [taskName, setTaskName] = useState("");
   const [progressTaskId, setProgressTaskId] = useState("");
-  //   console.log(
-  //     "🚀 ~ file: Dashboard.tsx ~ line 9 ~ progressTaskId",
-  //     progressTaskId
-  //   );
 
   useEffect(() => {
     const getTaskAPI = async () => {
-      const newTasks = await getTasks();
-      console.log(
-        "🚀 ~ file: Dashboard.tsx ~ line 17 ~ getTaskAPI ~ newTasks",
-        newTasks
-      );
-      setTasks(newTasks);
+      await getSnapTasks(setTasks);
+
+      console.log("get SnapShot");
     };
     getTaskAPI();
+    console.log("useEffect called!");
   }, []);
 
-  const createTaskAPI = async (taskName: string) => {
+  // handle
+  const handleCreateTask = async (taskName: string) => {
     const progressId = await createTask(taskName);
     console.log(
       "🚀 ~ file: Dashboard.tsx ~ line 11 ~ createTaskAPI ~ progressId",
       progressId
     );
     setProgressTaskId(progressId);
+    console.log(tasks);
     console.log("create New Task!");
     setTaskName("");
   };
-
-  const testHandle = () => {
-    console.log(tasks[0].createdAt);
+  const handleDeleteTask = async (id: number) => {
+    await deleteTask(id);
+    console.log("Success Delete!");
   };
+
   return (
     <>
       <Header />
       <Box px={4} h="calc(100vh - 60px)" overflowY="scroll">
         <Box w="90%" mx="auto">
-          <Box mt={8}>
-            <TableContainer>
-              <Table size="sm">
-                <Thead>
-                  <Tr>
-                    <Th>TaskName</Th>
-                    <Th>Start</Th>
-                    <Th>END</Th>
-                    <Th>TOTAL</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  {tasks?.map((task) => {
-                    return (
-                      <Tr key={task.id}>
-                        <Td>{task.content}</Td>
-                        {/* <Td>{task.createdAt}</Td> */}
-                        <Td>
-                          {new Date(task.createdAt.toDate()).toLocaleString()}
-                        </Td>
-                        <Td>16:53</Td>
-                        <Td>2:23</Td>
-                      </Tr>
-                    );
-                  })}
-                </Tbody>
-              </Table>
-            </TableContainer>
-          </Box>
           <Box mt={8}>
             <Box>
               <Text>タスク名</Text>
@@ -103,13 +71,47 @@ const Dashboard: React.FC = () => {
               />
             </Box>
             <Flex mt={4} justifyContent="space-around">
-              <Button w="40%" onClick={() => createTaskAPI(taskName)}>
+              <Button w="40%" onClick={() => handleCreateTask(taskName)}>
                 START
               </Button>
-              <Button w="40%" onClick={testHandle}>
-                STOP
-              </Button>
+              <Button w="40%">STOP</Button>
             </Flex>
+          </Box>
+
+          <Box mt={8}>
+            <TableContainer>
+              <Table size="sm">
+                <Thead>
+                  <Tr>
+                    <Th>TaskName</Th>
+                    <Th>Start</Th>
+                    <Th>END</Th>
+                    <Th>TOTAL</Th>
+                    <Th>DELETE</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {tasks?.map((task) => {
+                    return (
+                      <Tr key={task.id}>
+                        <Td>{task.content}</Td>
+                        {/* <Td>{task.createdAt}</Td> */}
+                        <Td>
+                          {new Date(task.createdAt?.toDate()).toLocaleString()}
+                        </Td>
+                        <Td>16:53</Td>
+                        <Td>2:23</Td>
+                        <Td>
+                          <Button onClick={() => handleDeleteTask(task.id)}>
+                            Del
+                          </Button>
+                        </Td>
+                      </Tr>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
           </Box>
         </Box>
       </Box>
--- src/service/api.tsx
+++ src/service/api.tsx
@@ -2,23 +2,35 @@ import { db } from "./firebase";
 import {
   addDoc,
   collection,
-  getDocs,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
   serverTimestamp,
 } from "firebase/firestore";
 
-export const getTasks = async () => {
-  let tasks: any[] = [];
-  const tasksRef = collection(db, "tasks");
-  const querySnapshot = await getDocs(tasksRef);
+// export const getTasks = async () => {
+//   let tasks: any[] = [];
+//   const tasksRef = collection(db, "tasks");
+//   const querySnapshot = await getDocs(tasksRef);
 
-  querySnapshot.forEach((doc) => {
-    tasks.push({
-      id: doc.id,
-      content: doc.data().content,
-      createdAt: doc.data().createdAt,
-    });
+//   querySnapshot.forEach((doc) => {
+//     tasks.push({
+//       id: doc.id,
+//       content: doc.data().content,
+//       createdAt: doc.data().createdAt,
+//     });
+//   });
+//   return tasks;
+// };
+
+export const getSnapTasks = async (setFn: any) => {
+  const taskData = collection(db, "tasks");
+  const q = query(taskData, orderBy("createdAt", "desc"));
+  onSnapshot(q, (querySnapShot) => {
+    setFn(querySnapShot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
   });
-  return tasks;
 };
 
 export const createTask = async (content: string) => {
@@ -27,4 +39,8 @@ export const createTask = async (content: string) => {
     createdAt: serverTimestamp(),
   });
   return docRef.id;
+};
+
+export const deleteTask = async (id: number) => {
+  await deleteDoc(doc(db, "tasks", id.toString()));
 };
